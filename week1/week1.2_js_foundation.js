cbd78d459df92049646307ef10b8ce55040b6195
highnitin <nitinjoshi983@gmail.com>
2024-04-22 00:38:45 +0530

    added week1 learnings

---
+++ week1/week1.2_js_foundation.js
@@ -0,0 +1,6 @@
+console.log("Hello Developer");
+console.log(a);
+
+// it will excute the log statement and hence js is a interpreted language.
+// c++ is a compiled language
+// interpreted languages can run line by line until they encounter error.
