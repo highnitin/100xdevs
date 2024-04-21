f598a4f3983bd97991bff154e9180b41795bc05e
highnitin <nitinjoshi983@gmail.com>
2024-04-22 03:18:28 +0530

    practiced basic js foundation (week1)

--- week1/week1.2_js_foundation.js
+++ week1/week1.2_js_foundation.js
@@ -1,6 +1,50 @@
-console.log("Hello Developer");
-console.log(a);
-
+// console.log("Hello Developer");
 // it will excute the log statement and hence js is a interpreted language.
 // c++ is a compiled language
 // interpreted languages can run line by line until they encounter error.
+// static vs dynamic languages
+// single threaded nature of js
+// custer module in js
+
+// var a = 1;
+// a = 2;
+// console.log(a);
+
+// let a = 1;
+// a = 2;
+// console.log(a);
+
+// const a = 1;
+// a = 2;
+// console.log(a);
+// let firstName = "Ramesh";
+// let isMarried = false;
+// let result;
+// isMarried == true ? (result = `Yes`) : (result = "No,");
+// console.log(result + " is married");
+
+// let sum = 0;
+// for (let i = 0; i < 1000000000000; i++) {
+// 	sum += i;
+// }
+// console.log(sum);
+
+// const n = 1000000000000;
+// const sum = (n * (n + 1)) / 2;
+// console.log(sum);
+
+// function sum(num1, num2) {
+// 	let result = num1 + num2;
+// 	return result;
+// }
+
+// function displayResult(data) {
+// 	console.log("Result of the sum is : " + data);
+// 	return data;
+// }
+
+// function displayResultPassive(data) {
+// 	console.log("Sum's result is : " + data);
+// }
+
+// console.log(displayResult(sum(3, 5)));
