134bf37b3b48001c3319b1bcf95f2f99b3201996
highnitin <nitinjoshi983@gmail.com>
2024-04-25 15:59:54 +0530

    updated learnings according to the lecture

---
+++ week1/week1.3_basic_js_apis.js
@@ -0,0 +1,87 @@
+// Week 1.3 | Basics JS APIs (Beginners)
+// apis here are not actual api but referred to built in properties by harkirat
+// auxillary helper methods
+
+//string
+// str.length,  not a function it is a property
+// indexof("hello", "l"); // function
+// lastIndexOf("hello", "l"); // function
+// str.slice(start,end)
+// replace("hello", "l");
+// split("hello", "l");
+// const str = "hello  ,  world";
+// const value = str.split(" ");
+// console.log(value);
+// str.trim();
+// toUpperCase()
+// "hello".toUpperCase();
+// "HELLO".toLowerCase();
+
+// number
+// parseInt - globalfunction
+
+// array
+// push()
+// pop()
+// shift()
+// unshift()
+// concat()
+// indexOf()
+// forEach()
+// map()
+// filter()
+// reduce()
+// slice()
+// splice()
+// sort()
+// reverse()
+
+// const value = [1, 2, 3];
+// function sum(num) {
+// 	console.log(num + 100);
+// }
+
+// value.forEach(sum);
+
+// class
+
+// class Animal {
+// 	constructor(aniName, breed, color, legs) {
+// 		this.aniName = aniName;
+// 		this.breed = breed;
+// 		this.color = color;
+// 		this.legs = legs;
+// 	}
+// 	static message() {
+// 		console.log(`Animal`);
+// 	}
+// 	info() {
+// 		console.log(
+// 			`${this.aniName} is of ${this.breed} breed in ${this.color} color with ${this.legs} legs`
+// 		);
+// 	}
+// }
+
+// const dog = new Animal("Tama", "Labu", "Golden", 4);
+// console.log(dog);
+
+// Animal.message();
+// dog.info();
+
+// Date and time function
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// // console.log(date.getTimezoneOffset());
+
+// const hh = date.getHours();
+// const hours = hh - 12;
+
+// console.log("Hello", hours);
+
+// json
+// math functions
+// object
---
+++ week1/week1.4_loop_function_callback.js
@@ -0,0 +1,2 @@
+// Week1.3 | Loops, Function and Callbacks
+console.log("Hello Developer");
