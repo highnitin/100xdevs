b14be43958339e2e1798a743d34cbde727a800fb
highnitin <nitinjoshi983@gmail.com>
2024-04-30 16:40:29 +0530

    started with 2.2 again

--- week2/2.2_Node.js_runtime_HTTP.js
+++ week2/2.2_Node.js_runtime_HTTP.js
@@ -2,3 +2,4 @@ console.log("Hello Developer");
 
 // ECMAscript
 // learn more at - https://tc39.es/ecma262/#sec-numbers-and-dates
+// long story short ecma script is a guiline or the rules for scrpiting language
