b5c4bbf2298fe5307b494f87e7044fa16d71a66f
highnitin <nitinjoshi983@gmail.com>
2024-05-07 17:26:30 +0530

    basic bash terminal session notes

---
+++ week2/2.3_Bash_terminal.js
@@ -0,0 +1,11 @@
+// commands
+
+// pwd - print working directory
+// cd - change directory
+// ls - list of directories present inside the folder
+// mkdir - make directory
+// cd .. - change directory to one level down
+// vi - vim editor
+// mv - move file/files
+// cp - copy file/files
+// cp - copy -r file/files
