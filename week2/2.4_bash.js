d6b9f46441a104eb7b5cc59c49dcbbbdafd3d656
highnitin <nitinjoshi983@gmail.com>
2024-05-12 02:30:54 +0530

    added few bash commands

--- week2/2.4_bash.js
+++ week2/2.4_bash.js
@@ -6,3 +6,26 @@
 // ls - lr
 // ls - la
 // try all these commands in git bash
+// ls * .js
+// ls -la week2
+// ls -R week2
+//ls -lR | grep .js
+//ls -lR | grep .json
+// ls wee* - using wilcard and regex for search
+// ls .. - check list of parent
+// cat index.js - to view content
+// cat > index.js - to add content to the file
+// cat >> index.js - to append content to the file
+// cntrl + t - to save the content added/appended
+// mkdir test && cd test - create and change
+// mkdir -p test/folder4
+// mv filename1 filename2 - rename file
+// mv filename1 folder1/filename2 - move and rename file
+// cp folder1/file1 folder2
+// cp -r folder1/file1 folder2
+// rm filename - remove file
+// rm -r folder - remove folder/directory
+//
+//
+//
+//
---
+++ week2/tempo.js
@@ -0,0 +1,5 @@
+console.log()[D"Hello Developer")
+let a = 1;
+console.log(a)
+a = 2;
+console.log(a)
