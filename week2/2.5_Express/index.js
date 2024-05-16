6208e4f973f983a36fdef2f5177e596a746deed8
highnitin <nitinjoshi983@gmail.com>
2024-05-16 23:01:35 +0530

    worked on express

--- week2/2.5_Express/index.js
+++ week2/2.5_Express/index.js
@@ -9,9 +9,11 @@ function squareRoot(n) {
 app.get("/", function (request, response) {
 	const n = request.query.n;
 	const result = squareRoot(n);
+	console.log(result.toString());
 	console.log(n);
-	response.send(`Square Root of ${n} is ${result.toString()}`);
+	response.send(
+		`Square Root of ${n ?? 1} is ${isNaN(result) ? 1 : result.toString()}`
+	);
 });
 
-// console.log(app.query);
 app.listen(3000);
